cac6db6fb708a3477c48307b440a1bf95022d699
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-13 19:46:04 -0300

    Instalação dos pacotes npm @mui/material @emotion/react @emotion/styled   /  colocado o padding no container e importado o container e o box do material ui
    Instalação dos pacotes npm @mui/material @emotion/react @emotion/styled   /  colocado o padding no container e importado o container e o box do material ui.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import './Calculator.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function Calculator() {
     return (
+        <>
+        <Box m={5}/>
+        <Container maxWidth="xs">
         <div className="wrapper">
             <button>1</button>
             <button>2</button>
@@ -24,5 +29,7 @@ export default function Calculator() {
             <button>-</button>
             <button>=</button>
         </div>
+        </Container>
+        </>
     )
 }
