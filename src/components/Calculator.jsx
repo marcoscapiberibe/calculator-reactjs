2b2c9fe3eaa060fee712f4c007607703e0af6d63
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-13 18:53:36 -0300

    Criação e estilização dos outros botões
    Criação e estilização dos outros botões

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,8 +3,26 @@ import './Calculator.css';
 
 export default function Calculator() {
     return (
-        <div>
+        <div className="wrapper">
             <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            <button>,</button>
+            <button>AC</button>
+            <button>+/-</button>
+            <button>%</button>
+            <button>/</button>
+            <button>X</button>
+            <button>+</button>
+            <button>-</button>
+            <button>=</button>
         </div>
     )
 }
