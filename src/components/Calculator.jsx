3b63998798df5ddf51662b7747995e09d590f7f0
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-14 11:20:02 -0300

    Criação do hook useState para armazenar os valores obtidos pela função de input criada
    Criação do hook useState para armazenar os valores obtidos pela função de input criada

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import './Calculator.css';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 export default function Calculator() {
+    const [number, setNumber] = useState(0);
+
+    function inputNumber(e) {
+        let input = e.target.value;
+        setNumber(input);
+    }
+
+
     return (
         <>
         <Box  m={5}/>
         <Container maxWidth="xs">
         <div className="wrapper">
+            <h1 className="result">{number}</h1>
             <button className="color--two">AC</button>
             <button className="color--two">+/-</button>
             <button className="color--two">%</button>
             <button className="color--one">÷</button>
-            <button className="color--three">7</button>
-            <button className="color--three">8</button>
-            <button className="color--three">9</button>
+            <button className="color--three" onClick={inputNumber} value={7}>7</button>
+            <button className="color--three" onClick={inputNumber} value={8}>8</button>
+            <button className="color--three" onClick={inputNumber} value={9}>9</button>
             <button className="color--one">X</button>
-            <button className="color--three">4</button>
-            <button className="color--three">5</button>
-            <button className="color--three">6</button>
+            <button className="color--three" onClick={inputNumber} value={4}>4</button>
+            <button className="color--three" onClick={inputNumber} value={5}>5</button>
+            <button className="color--three" onClick={inputNumber} value={6}>6</button>
             <button className="color--one">-</button>
-            <button className="color--three">1</button>
-            <button className="color--three">2</button>
-            <button className="color--three">3</button>
+            <button className="color--three" onClick={inputNumber} value={1}>1</button>
+            <button className="color--three" onClick={inputNumber} value={2}>2</button>
+            <button className="color--three" onClick={inputNumber} value={3}>3</button>
             <button className="color--one">+</button>
             <button className="color--three">0</button>
             <button className="color--two">,</button>
