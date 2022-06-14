ceece0eda528bfcdb12fdfeeaea9e0eaea3b7b7e
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-14 12:48:01 -0300

    Criação e estilização do componente Footer
    Criação e estilização do componente Footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
+    <>
     <div className="App">
       <Calculator />
     </div>
+    <div>
+      <Footer />
+    </div>
+    </>
   );
 }
 
