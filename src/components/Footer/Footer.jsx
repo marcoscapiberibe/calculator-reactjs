7ce26902870cda6ab1ee6ebd7b7eb664fc652afa
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-14 19:10:17 -0300

    Criação e estilização do Footer
    Criação e estilização do Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Footer/Footer.css';
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="rodape__desenvolvido">
+                <p>Desenvolvido com React JS por Marcos Capiberibe</p>
+                <p>Copyright © 2022</p>
+            </div>
+        </footer>
+    );
+}
